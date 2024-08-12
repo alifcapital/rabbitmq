ecb71ff15d1eb4bc29045f9c16e342a2a0dfae88
Yorzoda <yorzoda.behzod@gmail.com>
2024-08-12 14:27:02 +0500

    add openTelemetry middlewares

--- go.mod
+++ go.mod
@@ -1,10 +1,20 @@
 module github.com/alifcapital/rabbitmq
 
-go 1.20
+go 1.21
+
+toolchain go1.22.5
 
 require (
 	github.com/google/uuid v1.3.1
-	github.com/rabbitmq/amqp091-go v1.9.0
+	github.com/rabbitmq/amqp091-go v1.10.0
 )
 
 require github.com/opentracing/opentracing-go v1.2.0
+
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+)
--- mqutils/mids.go
+++ mqutils/mids.go
@@ -5,11 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/alifcapital/rabbitmq"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/opentracing/opentracing-go/log"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	trace2 "go.opentelemetry.io/otel/trace"
+
+	"github.com/alifcapital/rabbitmq"
 )
 
 type Middleware func(next rabbitmq.IConsumer) rabbitmq.IConsumer
@@ -71,6 +76,63 @@ func NewConsumerTraceLoggerMid() Middleware {
 			span.LogFields(log.String("id", msg.MessageId))
 			span.LogFields(log.String("body", string(msg.Body)))
 
+			next.Consume(newCtx, msg)
+		})
+	}
+}
+
+// NewOpenTelemetryMiddleware openTelemetry tracer for rabbitmq
+func NewOpenTelemetryMiddleware() Middleware {
+	return func(next rabbitmq.IConsumer) rabbitmq.IConsumer {
+		return rabbitmq.ConsumerFunc(func(ctx context.Context, msg amqp.Delivery) {
+			var span trace2.Span
+			var tracerCtx context.Context
+
+			tracer := otel.Tracer("openTelemetry-amqp-tracer")
+
+			spanName := fmt.Sprintf("|consume|%s|%s", msg.Exchange, msg.RoutingKey)
+			bagItemsJson, ok := msg.Headers[opentracingData].(string)
+			if ok {
+				bagItems := map[string]string{}
+				_ = json.Unmarshal([]byte(bagItemsJson), &bagItems)
+
+				// Extract context from carrier
+				propagator := propagation.TraceContext{}
+				parentCtx := propagator.Extract(ctx, propagation.MapCarrier(bagItems))
+
+				// Start span from extracted context
+				tracerCtx, span = tracer.Start(parentCtx, spanName, trace2.WithSpanKind(trace2.SpanKindConsumer))
+			} else {
+				// Start a new span if no context is present
+				tracerCtx, span = tracer.Start(ctx, spanName, trace2.WithSpanKind(trace2.SpanKindConsumer))
+			}
+			defer span.End()
+
+			// Add attributes instead of log fields
+			span.SetAttributes(attribute.String("message_id", msg.MessageId))
+
+			// Consume the message
+			next.Consume(tracerCtx, msg)
+		})
+	}
+}
+
+func NewConsumerOpenTelemetryTraceLoggerMid() Middleware {
+	return func(next rabbitmq.IConsumer) rabbitmq.IConsumer {
+		return rabbitmq.ConsumerFunc(func(ctx context.Context, msg amqp.Delivery) {
+			tracer := otel.Tracer("openTelemetry-amqp-tracer") // Create tracer
+
+			// Start a new span from the context
+			newCtx, span := tracer.Start(ctx, "LOG_MESSAGE", trace2.WithSpanKind(trace2.SpanKindConsumer))
+			defer span.End()
+
+			// Add attributes to the span
+			span.SetAttributes(
+				attribute.String("id", msg.MessageId),
+				attribute.String("body", string(msg.Body)),
+			)
+
+			// Consume the message with the new context
 			next.Consume(newCtx, msg)
 		})
 	}
